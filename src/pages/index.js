753edc8474e888c3bf8615fb78f6db7846f7d40e
yunggenius <yunggenius4@gmail.com>
2019-06-30 12:47:46 +0100

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default () => (
+<div style={{ margin: `5rem auto`, maxWidth: 500 }}>    
+
+  <h1>Hello!</h1>    
+
+  <p>This is an attempt to somthing amazing!</p> 
+          Visit The      <Link to="/blog">Blog</Link>  
+  </div>
+)
---
+++ src/pages/portfolio.js
@@ -0,0 +1,14 @@
+import React from "react"
+// import { Link, graphql } from "gatsby"
+
+
+// class BlogIndex extends React.Component {
+//   render() {
+//     const { data } = this.props
+//     const siteTitle = data.site.siteMetadata.title
+//     const posts = data.allMarkdownRemark.edges
+// }
+// }
+
+// export default () => <link ='http://localhost:8000'>Home</link> 
+export default () => <div> Yinka's Portfolio </div> 
