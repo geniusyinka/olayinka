d86b07fdf30d5a870aa8dd148f723ddcbcd47492
yunggenius <yunggenius4@gmail.com>
2019-07-02 18:38:58 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,8 +57,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `yinka.com.ng`,
+        short_name: `yinka.com.ng`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Layout from "../components/layout"
+
 export default () => (
 <div style={{ margin: `5rem auto`, maxWidth: 500 }}>    
 
